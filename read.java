cc5b810a9cdf71b6eba8b25b477810c47268fc77
zy <you@example.com>
2016-06-21 10:25:44 +0800

    new

--- read.java
+++ read.java
@@ -1,14 +1,3 @@
 that is nice
 this is change1
 this is change2
-this is change3
-this is change4
-this is change4
-this is change4
-this is change4
-this is change5
-
-in fix change1
-
-in master change1
-
